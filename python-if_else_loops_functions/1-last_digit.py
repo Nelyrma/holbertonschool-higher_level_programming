faf340a3cfd1a911dfc7441f6255403f8c5a0a28
marlyne <=>
2023-01-17 13:24:24 +0100

    Last digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -10,6 +10,6 @@ string = f"Last digit of {number} is {last_digit}"
 if last_digit > 5:
     print(f"{string} and is greater than 5")
 elif last_digit < 6 and last_digit != 0:
-    print(f"{string} is and is less than 6 and not 0")
+    print(f"{string} and is less than 6 and not 0")
 else:
     print(f"{string} and is 0")
