6cfa5d25fecc1f038d6e59f668bb8ec4c301e694
marlyne <=>
2023-01-18 10:58:34 +0100

    FizzBuzz

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for nbr in range(1, 100 +1):
+        if nbr % 3 == 0 and nbr % 5 == 0:
+            msg = "FizzBuzz"
+        elif nbr % 3 == 0:
+            msg = "Fizz"
+        elif nbr % 5 == 0:
+            msg = "Buzz"
+        else:
+            msg = str(nbr)
+
+        print("{}".format(msg), end=" ")
