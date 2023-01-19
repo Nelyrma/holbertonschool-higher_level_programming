f50a53282404c3c9a07255f959e4029fd750bf3c
marlyne <=>
2023-01-19 01:06:14 +0100

    number and list of arguments

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from sys import argv
+
+    length = len(argv)
+    
+    if (length - 1) == 1:
+        print("1 argument:")
+    else:
+        print(f"{length - 1} arguments:" if (length - 1) else "0 arguments.")
+    for i in range(1, length):
+        print(f"{i}: {argv[i]}")
