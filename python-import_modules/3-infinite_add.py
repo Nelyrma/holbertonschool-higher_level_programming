95923960ee0ce1694a842c0258ec1f8a2f9ee194
marlyne <=>
2023-01-19 15:41:01 +0100

    infinite addition

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from sys import argv
+    add = 0
+
+    if len(argv) > 1:
+        for x in range (1, len(argv)):
+            add += int(argv[x])
+        print(add)
+    else:
+        print(0)
