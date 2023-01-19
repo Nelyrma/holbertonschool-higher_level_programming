357860e43b61592e64aa041135dee910607269c3
marlyne <=>
2023-01-19 15:42:02 +0100

    infinite addition

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     add = 0
 
     if len(argv) > 1:
-        for x in range (1, len(argv)):
+        for x in range(1, len(argv)):
             add += int(argv[x])
         print(add)
     else:
