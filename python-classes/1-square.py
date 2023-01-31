91ddd7fe483c62ab0389b58b1fd1875beca23184
marlyne <=>
2023-01-31 10:05:27 +0100

    Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Define a square by private intance attribute
+"""
+
+
+class Square:
+    """
+    size: size of a square
+    """
+    def __init__(self, size):
+        self.__size = size
