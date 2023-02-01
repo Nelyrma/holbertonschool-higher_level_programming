5aa48f056d315f00ff04f19cebf435c5c9449af4
marlyne <=>
2023-02-01 18:03:58 +0100

    Area and perimeter

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -65,4 +65,7 @@ class Rectangle:
         """
         returns the perimeter of a rectangle
         """
-        return (self.__width + self.__height) * 2
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (self.__width + self.__height) * 2
