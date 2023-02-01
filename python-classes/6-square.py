d350f9cf58679eaab1e77ba1f6f73b381a5ba64b
marlyne <=>
2023-02-01 14:10:25 +0100

    Coordinates of a square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -67,8 +67,8 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            print("\n" * self.__position[1], end="")
-            print("\n".join([" " * self.__position[0] +
-                             "#" * self.__size
-                             for rows in range(self.__size)]))
+            for i in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                print(f'{self.__position[0] * " "}{self.__size * "#"}')
 
