43f6a251b94991e04ae9faa5b2bbdf9085809619
marlyne <=>
2023-02-01 18:28:18 +0100

    String representation

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -76,5 +76,5 @@ class Rectangle:
         """
         if self.__width == 0 or self.__height == 0:
             return ""
-        string = "\n".join(["#" * self.__width for rows in range(self.__height)])
-        return string
+        rect = "\n".join(["#" * self.__width for rows in range(self.__height)])
+        return rect
