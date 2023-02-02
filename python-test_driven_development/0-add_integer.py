64203a8a5d9549534b740f2cabbdf865aaa56e5a
marlyne <=>
2023-02-03 00:33:11 +0100

    Integers addition

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+contains a function that adds two integers
+"""
+
+
+def add_integer(a, b=98):
+    """
+    returns an integer: a + b 
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
+
