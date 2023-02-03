9af1fc0405deeb05eca28ef1a703d2d037bc79e1
marlyne <=>
2023-02-03 20:52:18 +0100

    Integers addition

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,7 +6,7 @@ contains a function that adds two integers
 
 def add_integer(a, b=98):
     """
-    returns an integer: a + b 
+    returns an integer: a + b
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
@@ -14,4 +14,3 @@ def add_integer(a, b=98):
         raise TypeError("b must be an integer")
     else:
         return int(a) + int(b)
-
