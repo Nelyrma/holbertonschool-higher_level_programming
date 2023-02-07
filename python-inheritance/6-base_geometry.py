d8cd79ca3c3bd97d9f3dcd7208a24e75c073512b
marlyne <=>
2023-02-07 20:22:45 +0100

    Method that raises an Exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+contains a BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    Defines a class BaseGeometry
+    Functions:
+        area(self)
+    """
+    def area(self):
+        """
+        raises an exception with a message
+        """
+        raise Exception("area() is not implemented")
