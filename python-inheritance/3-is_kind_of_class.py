7533b7278921414c27c396ea0c804742fffd93bc
marlyne <=>
2023-02-07 19:04:15 +0100

    Checks if an obj is an instance

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+contains the is_kind_of_class method
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    checks if the object is an instance
+    """
+    return (isinstance(obj, a_class))
