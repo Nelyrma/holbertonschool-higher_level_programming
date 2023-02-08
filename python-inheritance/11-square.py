794034d8c901fe7bf82cda2590a3552438a54a7b
marlyne <=>
2023-02-09 00:12:34 +0100

    Square#2

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+module for subclass Square that inherits from Rectangle
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Defines a subclass Square"""
+    def __init__(self, size):
+        """constructor for square"""
+        self.integer_validator('size', size)
+
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """returns the area of the square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """returns a square description"""
+        string = "[Square] {}/{}".format(self.__size, self.__size)
+        return string
