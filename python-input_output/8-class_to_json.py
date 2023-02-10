f7f6b647bb1c69fe9bdbeeae5ef6811d697a1adb
marlyne <=>
2023-02-10 15:08:29 +0100

    Class to JSON

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+contains a function that returns the dictionary
+description with simple data structure (list,
+dictionary, string, integer and boolean) for JSON
+serialization of an object
+"""
+
+import json
+
+
+def class_to_json(obj):
+    """returns the dictionary description"""
+    return obj.__dict__
