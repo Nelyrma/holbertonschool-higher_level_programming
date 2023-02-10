eda57cbea06bb4622b6726e38dcf9d4b2f1acd8c
marlyne <=>
2023-02-10 22:34:36 +0100

    Student to JSON

---
+++ python-input_output/9-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""contains a class Student"""
+
+
+class Student:
+    """defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is not None:
+            i = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    m[i] = self.__dict__[i]
+            return m
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        for i in json:
+            self.__dict__[i] = json[i]
