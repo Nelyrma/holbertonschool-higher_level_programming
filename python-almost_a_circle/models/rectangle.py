591101ee921c48664fe4da96e6a8ad8df74cc588
marlyne <=>
2023-02-14 11:07:41 +0100

    Update the class Rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -90,3 +90,8 @@ class Rectangle(Base):
     def area(self):
         """returns the area of the rectangle"""
         return self.__width * self.__height
+
+    def display(self):
+        """prints in stdout the rectangle instance with the character #"""
+        rect = "\n".join(["#" * self.__width for rows in range(self.__height)])
+        return rect
