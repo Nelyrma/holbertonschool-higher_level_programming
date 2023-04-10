05702b828ea315d82a4795511452c1eed0597975
marlyne <=>
2023-04-10 18:33:12 +0200

    loop to languages

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // print 3 lines using an array of string and a loop
 
-let arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
+  console.log(arr[i]);
 }
