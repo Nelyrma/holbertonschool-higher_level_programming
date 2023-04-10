e197dfc2189f8de2004d357be6955c0e56db12ad
marlyne <=>
2023-04-10 23:46:06 +0200

    print a square

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// print a square
+
+const arg = process.argv[2];
+
+const size = Number(arg);
+
+if (Number.isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    let line = '';
+    for (let j = 0; j < size; j++) {
+        line += 'X';
+    }
+    console.log(line);
+  }
+}
