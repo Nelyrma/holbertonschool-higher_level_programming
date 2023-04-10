021c29e73583795d6ab0af60a4345f375a98828d
marlyne <=>
2023-04-10 17:53:18 +0200

    create a sentence

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -3,10 +3,10 @@
 
 const args = process.argv.slice(2);
 
-if (args.length === 0){
-    console.log('undefined is undefined');
+if (args.length === 0) {
+  console.log('undefined is undefined');
 } else if (args.length === 1) {
-    console.log(args[0] + ' is undefined')
+  console.log(args[0] + ' is undefined')
 } else {
-    console.log(args[0] + ' is ' + args[1]);
+  console.log(args[0] + ' is ' + args[1]);
 }
