82182b85b65f0fa995d02a1babad82ff357e7cff
marlyne <=>
2023-04-10 23:48:08 +0200

    print a square

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -11,7 +11,7 @@ if (Number.isNaN(size)) {
   for (let i = 0; i < size; i++) {
     let line = '';
     for (let j = 0; j < size; j++) {
-        line += 'X';
+      line += 'X';
     }
     console.log(line);
   }
