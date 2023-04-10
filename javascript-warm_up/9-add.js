ef306b5029d816b2b9e134138caea324552c3e6d
marlyne <=>
2023-04-11 00:19:13 +0200

    print the addition of two integers

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// add two integers
+
+function add (a, b) {
+    return a + b;
+}
+  
+const num1 = Number(process.argv[2]);
+const num2 = Number(process.argv[3]);
+  
+console.log(add(num1, num2));
