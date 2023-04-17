efe17ce7c4c0a16d15a46b560c170ce7d0a625bf
marlyne <=>
2023-04-18 01:28:58 +0200

    print the number of movies of a character

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+
+  const films = JSON.parse(body).results;
+
+  let countFilm = 0;
+  for (const film of films) {
+    for (const character of film.characters) {
+      if (character.includes('18')) {
+        countFilm++;
+        break;
+      }
+    }
+  }
+  console.log(countFilm);
+});
