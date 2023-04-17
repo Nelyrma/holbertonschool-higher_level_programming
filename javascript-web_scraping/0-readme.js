00ba8a52b1e7ac5f1e871bb61b3c76f7bbb4b89c
marlyne <=>
2023-04-17 21:16:14 +0200

    read and print the content of a file

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const fs = require('fs')
+
+const filepath = process.argv[2]
+
+fs.readFile(filepath, 'utf-8', function(err, data) {
+    if (err) throw err;
+    console.log(data);
+});
