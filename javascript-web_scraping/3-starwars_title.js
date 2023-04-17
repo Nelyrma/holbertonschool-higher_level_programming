d038522a40bb3e3887acdb5b0bfd3388c6587376
marlyne <=>
2023-04-18 00:43:04 +0200

    star wars movie title

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 
-const url = 'https://swapi-api.hbtn.io/api/films/:id' + process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
 request(url, function (err, response, body) {
   console.log(JSON.parse(body).title || err);
