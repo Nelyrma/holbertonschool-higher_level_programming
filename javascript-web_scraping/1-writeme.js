bb5a29ebb6712296d95510f005c348aedf53bdbb
marlyne <=>
2023-04-17 21:42:38 +0200

    write a string to a file

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -6,6 +6,6 @@ const filepath = process.argv[2];
 
 const string = process.argv[3];
 
-fs.writeFile(filepath, string, 'utf-8', function(err) {
-    if (err) throw err;
+fs.writeFile(filepath, string, 'utf-8', function (err) {
+  if (err) throw err;
 });
